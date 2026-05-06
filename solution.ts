e8b712d2f8d9d381514381a95b5da071fb497798
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-06 20:43:28 +0600

    solved problem three

--- solution.ts
+++ solution.ts
@@ -2,17 +2,35 @@ const filterEvenNumbers = (arr: number[]): number[] => {
   return arr.filter((num) => num % 2 === 0);
 };
 
-/**
- * Problem 2:
-Write a function reverseString that takes a string as input and returns the reversed version of that string.
-
-// Sample Input:
-reverseString("typescript");
-
-// Sample Output:
-"tpircsepyt";
- */
 const reverseString = (str: string): string => {
   return `"${str.split("").reverse().join("")}"`;
 };
-console.log(reverseString("typescript"));
+
+/**
+ * Problem 3:
+Define a union type StringOrNumber and create a function checkType that uses type guards to return "String" if the input is a string or "Number" if the input is a number.
+
+// Sample Input 1:
+checkType("Hello");
+
+// Sample Output 1:
+"String";
+
+// Sample Input 2:
+checkType(42);
+
+// Sample Output 2:
+"Number";
+ */
+
+type StringOrNumber = string | number;
+const checkType = (val: StringOrNumber): "String" | "Number" => {
+  if (typeof val === "string") {
+    return "String";
+  }
+  return "Number";
+};
+
+console.log(checkType("Hello"));
+console.log(checkType(42));
+console.log(checkType(true));
