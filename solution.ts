4af1e0036852afd0b332fd2327ea6f059cf8b084
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-06 20:20:08 +0600

    solved problem one

---
+++ solution.ts
@@ -0,0 +1,17 @@
+/**
+ * Problem 1:
+Create a TypeScript function filterEvenNumbers that accepts an array of numbers and returns a new array containing only the even numbers.
+
+// Sample Input:
+filterEvenNumbers([1, 2, 3, 4, 5, 6])
+
+// Sample Output:
+[2, 4, 6]
+ */
+
+const filterEvenNumbers = (arr: number[]): number[] => {
+  return arr.filter((num) => num % 2 === 0);
+};
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+console.log(filterEvenNumbers([1, 3, 5]));
+console.log(filterEvenNumbers([]));
